fa02973308f153525ba978c06842435c5a4b1f50
LuqmanAliSyed-7855 <lsyed7855@ucumberlands.edu>
2025-07-16 19:20:01 +0530

    Delete extract_min task.py

--- extract_min task.py
+++
@@ -1,6 +0,0 @@
-import heapq
-
-# Remove and return the task with the lowest priority value
-def extract_min_task(heap):
-    # heappop removes the smallest element and restores heap property
-    return heapq.heappop(heap)
